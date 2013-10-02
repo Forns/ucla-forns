047b4e606238c3d242811742bec0b9b57d77e9ef
Forns <forneyandrew@gmail.com>
2013-10-02 15:36:59 -0700

    - Still working on lecture 1!

---
+++ js/display/general/general-display.js
@@ -0,0 +1,67 @@
+/**
+ * general-display.js
+ * 
+ * Javascript-mediated display elements for most site pages
+ */
+$(function () {
+  var scrollspy = $("#scrollspy"),
+      presentationMode = false,
+      presentationClass = "presentation-display",
+      glyphiconMap = {
+        ".question": "glyphicon-question-mark",
+        ".definition": "glyphicon-chevron-right",
+        ".answer": "glyphicon-chevron-right"
+      };
+  
+  // Set up ScrollSpy elements
+  $(".scrollspy-element")
+    .each(function () {
+      scrollspy.append(
+        "<li><a href='#" + $(this).attr("id") + "'>" + $(this).attr("scrollspy-title") + "</a></li>"
+      );
+    });
+    
+  // Refresh scrollspy
+  $('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh');
+  });
+  
+  // Set up presentation mode
+  $(document)
+    .on("keypress", function (e) {
+      if (e.keyCode === 80) {
+        // If we're already in presentation mode, turn it off...
+        if (presentationMode) {
+          $("." + presentationClass)
+            .removeClass(presentationClass);
+        
+        // Otherwise, we need to apply presentation mode
+        } else {
+          // Soooo select all descendants of presentation content
+          $(".presentation-content")
+            .find("p, pre")
+            .addClass(presentationClass);
+        }
+        presentationMode = !presentationMode;
+      }
+    });
+  
+  // Configure display of the alert elements
+  for (var elements in glyphiconMap) {
+    $(elements)
+      .prepend("<span class='glyphicon " + glyphiconMap[elements] + "'></span>&nbsp");
+  }
+  
+  // Questions can be clicked on for the answer
+  $(".question")
+    .each(function () {
+      $(this)
+        .click(function () {
+          $(this)
+            .fadeOut(200, function () {
+              $("[name='" + $(this).attr("name") + "'].answer")
+                .fadeIn(200);
+            });
+        });
+    });
+});
