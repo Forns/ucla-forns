0407e09e72cd567c9ec24c217dbc5e871f0a21ee
Forns <forneyandrew@gmail.com>
2013-12-02 07:46:00 -0800

    - Goin to skewl!

--- projects/simpson/app/js/simpson.js
+++ projects/simpson/app/js/simpson.js
@@ -3,6 +3,24 @@
  * Includes Daggity-mediated functionality for Simpson's Paradox operations
  */
 
-window.setTimeout(function () {
-  console.log(Model);
-}, 1000);
+var simpsonInit = function () {
+  console.log(Model.dag);
+  
+  console.log(Model.dag.causalFlowGraph());
+  
+  
+  // List of outcome variables Y
+  //console.log(Model.dag.getTargets());
+  
+  // List of exposure variables X
+  // console.log(Model.dag.getSources());
+  
+  // Returns all nodes from X -> ... -> Y
+  // console.log(Model.dag.nodesOnCausalPaths());
+  
+  // The graph with backdoor from exposure to effect alone; no causal path from X -> Y
+  // console.log(Model.dag.backDoorGraph().toString());
+  
+  // Ordering from top node (fewest parents) to exposure and effect
+  // console.log(Model.dag.topologicalOrdering());
+};
