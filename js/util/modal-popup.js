9ce7c8bb5dd972ab775b788e492038ea46c312f5
Forns <forneyandrew@gmail.com>
2013-12-11 02:19:56 -0800

    - Intermediary commit working on Simpson

--- js/util/modal-popup.js
+++ js/util/modal-popup.js
@@ -4,8 +4,8 @@
  * Simple work-saver for making a bootstrap modal popup
  */
 
-var modalPopup = function (container, id, title, body, buttons, options) {
-      container = $(container);
+var modalPopup = function (container, id, title, body, buttons, options, display) {
+      container = jQuery(container);
       container
         .append(
           "<div id='" + id + "' data-backdrop='static' data-keyboard='false' class='modal fade' tabindex='-1' role='dialog' aria-labelledby='model-title' aria-hidden='true'>" +
@@ -25,7 +25,15 @@ var modalPopup = function (container, id, title, body, buttons, options) {
           "</div>"
         );
         
-      return $("#" + id).modal(options);
+      if(display && display.image) {
+        jQuery("#" + id + " .modal-dialog")
+          .addClass("modal-image");
+      } else {
+        jQuery("#" + id + " .modal-dialog")
+          .removeClass("modal-image");
+      }
+      
+      return jQuery("#" + id).modal(options);
     };
     
     // Frequent error-popup
@@ -34,7 +42,7 @@ var modalPopup = function (container, id, title, body, buttons, options) {
         container,
         id,
         title,
-        "<p><strong>Error:</strong> Sorry, looks like an error occurred on our side! If you continue to see this message, please contact the FMD administrators using the webform at the bottom of the page.</p>" +
+        "<p><strong>Error:</strong> Sorry, looks like an error occurred on our side! Please contact Andrew to report this problem... or not, that seems like a bit of an inconvenience.</p>" +
         "<p>Error Status: " + error + "</p>",
         "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>OK</button>"
       );
--- projects/simpson/app/js/simpson.js
+++ projects/simpson/app/js/simpson.js
@@ -11,14 +11,25 @@ var simpsonAnalysis = function () {
           pathCount = 0,
           sources = dag.getSources(),
           targets = dag.getTargets(),
+          minimalSeparators = GraphAnalyzer.listMsasTotalEffect(dag),
+          dontCondition = [],
           allLatentReport = "";
       
       // Let's make sure we can even run the rest of our algorithm before continuing
       if (dag.containsCycle()) {
         return {
           possible: "No; Cycle in graph",
-          condition: "N/A"
+          condition: "N/A",
+          code: 0
         };
+      }
+      
+      if (sources.length === 0 || targets.length === 0) {
+        return {
+          possible: "No; No exposure or outcome set",
+          condition: "N/A",
+          code: -1
+        }
       }
       
       // We'll start off by looking at every path between our sources and targets
@@ -36,7 +47,8 @@ var simpsonAnalysis = function () {
       if (pathCount <= 1) {
         return {
           possible: "No; one path from exposure to outcome",
-          condition: "N/A"
+          condition: "N/A",
+          code: 1
         };
       }
       
@@ -55,7 +67,8 @@ var simpsonAnalysis = function () {
                   return result;
                 })() +
               "</ul>",
-            condition: "N/A"
+            condition: "N/A",
+            code: 2
           };
         }
       });
@@ -65,33 +78,165 @@ var simpsonAnalysis = function () {
       }
       
       // Otherwise, it's possible and we should report where the correct answer is!
+      
+      // Check if we've accidentally conditioned on something along the causal path:
+      dag.nodesOnCausalPaths().each(function (n) {
+        if (dag.isAdjustedNode(n)) {
+          dontCondition.push(n.id);
+        }
+      });
+      
+      if (dontCondition.length) {
+        dontCondition = "{" + dontCondition.join(", ") + "}";
+      }
+      
       if (dag.activeBiasGraph().edges.length) {
         return {
           possible: "Yes",
           report: "sets",
-          condition: GraphAnalyzer.listMsasTotalEffect(dag)
+          condition: minimalSeparators,
+          dontCondition: dontCondition,
+          answer: "Bias requires resolution",
+          code: 3
         };
       } else {
+        var conditioned = [];
+        dag.getVertices().each(function (v) {
+          if (dag.isAdjustedNode(v)) {
+            conditioned.push(v);
+          }
+        });
+        
         return {
           possible: "Yes",
-          condition: "No"
+          condition: "No",
+          dontCondition: dontCondition,
+          answer: (minimalSeparators.intersect(conditioned).length) ? "Disaggregated Data" : "Aggregated Data",
+          code: 4
         };
       }
       
       // [TESTING] Default return
       return {
         possible: "N/A",
-        condition: "N/A"
+        condition: "N/A",
+        code: -2
       };
     },
 
     displaySimpsonInfo = function () {
-      console.log(Model.dag.activeBiasGraph());
       var results = simpsonAnalysis(),
           infoSimpsonPossible = $("info_simpson_possible"),
-          infoSimpsonCondition = $("info_simpson_condition");
+          infoButton = jQuery("#simpson_info_button"),
+          simpsonPopup = jQuery("#simpsonPopup"),
+          popupBody = simpsonPopup.find(".modal-body"),
+          
+          // Holds the report info... could probably be cleaned up but... meh
+          infoReport = 
+            // --------------------------------------------------------------------
+            // Nav tabs
+            "<ul class='nav nav-tabs'>" +
+              "<li class='active'><a href='#explanation' data-toggle='tab'>Explanation</a></li>" +
+              "<li><a href='#simulation' data-toggle='tab'>Simulation</a></li>" +
+              "<li><a href='#analysis' data-toggle='tab'>Analysis</a></li>" +
+            "</ul>" +
+            
+            // --------------------------------------------------------------------
+            // Nav panes
+            "<div class='tab-content'>" +
+            
+              // Explanation nav pane
+              "<div class='tab-pane active' id='explanation'>" +
+              
+                "<div class='table-responsive table-bordered'>" +
+                  "<table class='table'>" +
+                    "<thead>" +
+                    "</thead>" +
+                    "<tbody>" +
+                    
+                      "<tr class='" + ((results.code > 2) ? "success" : "danger") + "'>" +
+                        "<td><strong>Is it possible?<strong></td>" +
+                        "<td>" + results.possible + "</td>" +
+                      "</tr>" +
+                      
+                      ((results.code > 2) ? 
+                      (
+                        "<tr class='" + ((results.code === 3) ? "warning" : "success") + "'>" +
+                          "<td><strong>Unblocked backdoor?</strong></td>" +
+                          "<td>" +
+                            
+                            // Messy way to display the minimal separating set(s)
+                            (function () {
+                              var report = results.condition;
+                              if (results.code === 3) {
+                                report = [];
+                                for (var i = 0; i < results.condition.length; i++) {
+                                  report.push(
+                                    (function () {
+                                      var subRepo = [];
+                                      results.condition[i].each(function (j) {
+                                        subRepo.push(j.id);
+                                      });
+                                      return "{" + subRepo.join(", ") + "}";
+                                    })()
+                                  );
+                                }
+                                report = "Yes, adjust for (at least one of):<br/>" + report.join(", ");
+                              }
+                              return report;
+                            })() +
+                            
+                          "</td>" +
+                        "</tr>" +
+                        
+                        // Check for NOT conditioning along the causal path
+                        ((results.dontCondition.length) 
+                          ? 
+                            "<tr class='danger'>" +
+                              "<td><strong>Should NOT adjust for:</strong></td>" +
+                              "<td>" + results.dontCondition + "</td>" +
+                            "</tr>"
+                          : "") +
+                          
+                        // If all green, report where answer is
+                        ((!results.dontCondition.length && results.code === 4) 
+                          ? 
+                            "<tr class='success'>" +
+                              "<td><strong>TE of exposure on outcome in:</strong></td>" +
+                              "<td>" + results.answer + "</td>" +
+                            "</tr>"
+                          : "")
+                        
+                      ) : "") +
+                      
+                    "</tbody>" +
+                  "</table>" +
+                "</div>" +
+              
+              "</div>" +
+              
+              // Simulation nav pane
+              "<div class='tab-pane' id='simulation'>" +
+              "</div>" +
+              
+              // Analysis nav pane
+              "<div class='tab-pane' id='analysis'>" +
+              "</div>" +
+              
+            "</div>";
+          
+      infoSimpsonPossible.innerHTML = results.possible;
       
-      infoSimpsonPossible.innerHTML = "<p>" + results.possible + "</p>";
+      // Event handler for the explanation button
+      infoButton
+        .click(function () {
+          popupBody
+            .html(infoReport);
+          simpsonPopup
+            .modal("show");
+        });
+      
+      /*
       infoSimpsonCondition.innerHTML = 
         "<p>" + 
         ((results.report === "sets") 
@@ -111,6 +256,8 @@ var simpsonAnalysis = function () {
           })()
           : results.condition) + 
         "</p>";
+      */
+      
       
       // Bool value for whether node is adjusted for or not
       // console.log(Model.dag.isAdjustedNode(Model.dag.getVertex("Z1")));
@@ -136,3 +283,19 @@ var simpsonAnalysis = function () {
       // Ordering from top node (fewest parents) to exposure and effect
       // console.log(Model.dag.topologicalOrdering());
     };
+    
+jQuery(function () {
+  modalPopup(
+    "body",
+    "simpsonPopup",
+    "Simpson's Paradox",
+    "<p>Loading...</p>",
+    "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>",
+    {
+      show: false
+    },
+    {
+      image: true
+    }
+  );
+});
